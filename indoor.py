1dfb9d1f230cd526db3c8a7a98a50a9c323408a6
DanielSuth <167015888+DanielSuth@users.noreply.github.com>
2024-04-15 18:55:11 +0200

    Add files via upload
    Week 0 indoor

---
+++ indoor.py
@@ -0,0 +1,8 @@
+# Prompting the user for input
+userinput = input("What would you like to say ? ")
+
+# Converting the input to lowercase
+lowercaseinput = userinput.lower()
+
+# Outputting the lowercase input
+print(lowercaseinput)
